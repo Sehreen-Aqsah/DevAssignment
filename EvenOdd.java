cc0e1360031dc0bf8147cdaf8fff80bed2fd7434
Sehreen-Aqsah <aqsahsehreen@gmail.com>
2025-12-16 21:52:33 +0530

    EvenOdd Program file added

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+	public static void main(String[] args) {
+		int num = 17;
+		
+		if(num % 2 == 0) {
+			System.out.println("EVEN");
+		} else {
+			System.out.println("ODD");
+		}
+	}
+}
